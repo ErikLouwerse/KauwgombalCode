db7c84c14c85f86a4c98f0647299565d9023515b
ErikLouwerse <eriklouwers1@gmail.com>
2018-05-21 20:21:56 +0200

    Animetion update

--- Communicator1.java
+++ Communicator1.java
@@ -9,19 +9,27 @@ import gnu.io.SerialPortEvent;
 import gnu.io.SerialPortEventListener;
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.scene.paint.Color;
 
 public class Communicator1 implements SerialPortEventListener {
 
-    private int blauwcount = 0;
-    private int geelcount = 0;
-    private int groencount = 0;
-    private int roodcount = 0;
+    static int blauwcount = 0;
+    static int geelcount = 0;
+    static int groencount = 0;
+    static int roodcount = 0;
+    static int bakblauwcount = 0;
+    static int bakgeelcount = 0;
+    static int bakgroencount = 0;
+    static int bakroodcount = 0;
+    static int pakjes = 0;
+    private int r = 200;
 
     SerialPort serialPort;
 
     //The port we're going to use.
-    private String PORT_NAME[] = {"COM4"};
+    private String PORT_NAME[] = {"COM5"};
 
     //A BufferedReader which will be fed by a InputStreamReader converting the bytes into characters
     BufferedReader input;
@@ -74,6 +82,81 @@ public class Communicator1 implements SerialPortEventListener {
         }
     }
 
+    static void drawMachine() {
+        try {
+            //Kleuren sorteermachine
+            GUInew.gc.strokeRect(20, 200, 200, 100);
+
+            //Groene bakje, bal en counter
+            GUInew.gc.strokeRect(450, 215, 80, 70);
+            GUInew.gc.setFill(Color.GREEN);
+            GUInew.gc.fillOval(455, 232, 36, 36);
+            GUInew.gc.setFill(Color.BLACK);
+            GUInew.gc.fillText("X " + groencount, 495, 255);
+
+            //Rode bakje, bal en counter
+            GUInew.gc.strokeRect(425, 135, 80, 70);
+            GUInew.gc.setFill(Color.RED);
+            GUInew.gc.fillOval(430, 152, 36, 36);
+            GUInew.gc.setFill(Color.BLACK);
+            GUInew.gc.fillText("X " + roodcount, 470, 175);
+
+            //Blauwe bakje, bal en counter
+            GUInew.gc.strokeRect(425, 295, 80, 70);
+            GUInew.gc.setFill(Color.BLUE);
+            GUInew.gc.fillOval(430, 312, 36, 36);
+            GUInew.gc.setFill(Color.BLACK);
+            GUInew.gc.fillText("X " + blauwcount, 470, 335);
+
+            //Gele bakje, bal en counter
+            GUInew.gc.strokeRect(400, 55, 80, 70);
+            GUInew.gc.setFill(Color.YELLOW);
+            GUInew.gc.fillOval(405, 72, 36, 36);
+            GUInew.gc.setFill(Color.BLACK);
+            GUInew.gc.fillText("X " + geelcount, 445, 95);
+
+            //Restbakje
+            GUInew.gc.strokeRect(400, 375, 80, 70);
+            GUInew.gc.fillText("Restbak", 410, 415);
+
+            //Trechter
+            GUInew.gc.strokeLine(550, 55, 800, 185);
+            GUInew.gc.strokeLine(550, 365, 800, 235);
+            GUInew.gc.strokeLine(800, 185, 870, 185);
+            GUInew.gc.strokeLine(800, 235, 870, 235);
+
+            //Eindbakje
+            GUInew.gc.strokeRect(900, 150, 200, 120);
+            GUInew.gc.setFill(Color.GREEN);
+            GUInew.gc.fillOval(925, 215, 36, 36);
+            GUInew.gc.setFill(Color.BLACK);
+            GUInew.gc.fillText("X " + bakgroencount, 965, 240);
+            GUInew.gc.setFill(Color.RED);
+            GUInew.gc.fillOval(1005, 165, 36, 36);
+            GUInew.gc.setFill(Color.BLACK);
+            GUInew.gc.fillText("X " + bakroodcount, 1045, 190);
+            GUInew.gc.setFill(Color.BLUE);
+            GUInew.gc.fillOval(1005, 215, 36, 36);
+            GUInew.gc.setFill(Color.BLACK);
+            GUInew.gc.fillText("X " + bakblauwcount, 1045, 240);
+            GUInew.gc.setFill(Color.YELLOW);
+            GUInew.gc.fillOval(925, 165, 36, 36);
+            GUInew.gc.setFill(Color.BLACK);
+            GUInew.gc.fillText("X " + bakgeelcount, 965, 190);
+            
+            //Pakketjes klaar teller
+            if (pakjes > 0){
+            GUInew.gc.fillText("Aantal pakketjes klaar:  " + pakjes, 1250, 210);
+            } else {
+                GUInew.gc.fillText("Aantal pakketjes klaar: 0", 1200, 210);
+            }
+
+            Thread.sleep(20);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Communicator1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     //Handle an event on the serial port. Read the data and print it.
     @Override
     public void serialEvent(SerialPortEvent oEvent) {
@@ -82,146 +165,80 @@ public class Communicator1 implements SerialPortEventListener {
                 String inputLine = input.readLine();
                 Logboek.addRule(System.currentTimeMillis(), "(Arduino1): " + inputLine);
                 if (inputLine.equals("rood")) {
-                    roodcount++;
-                    for (int i = 0; i < 500; i = i + 4) {
+                    for (int i = 0; i < 200; i = i + 4) {
                         GUInew.gc.clearRect(0, 0, 5000, 5000);
                         GUInew.gc.setFill(Color.RED);
                         GUInew.gc.fillOval(i, 232, 36, 36);
-                        GUInew.gc.strokeRect(20, 200, 200, 100);
-                        GUInew.gc.strokeLine(220,225, 425, 145);
-                        GUInew.gc.strokeLine(220,275, 425, 195);
-                        //GUInew.gc.strokeRect(220, 225, 200, 50);
-                        GUInew.gc.strokeRect(450, 215, 80, 70);
-                        GUInew.gc.setFill(Color.GREEN);
-                        GUInew.gc.fillOval(455, 232, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + groencount, 495, 255);
-                        GUInew.gc.strokeRect(425, 135, 80, 70);
-                        GUInew.gc.setFill(Color.RED);
-                        GUInew.gc.fillOval(430, 152, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + roodcount, 470, 175);
-                        GUInew.gc.strokeRect(425, 295, 80, 70);
-                        GUInew.gc.setFill(Color.BLUE);
-                        GUInew.gc.fillOval(430, 312, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + blauwcount, 470, 335);
-                        GUInew.gc.strokeRect(400, 55, 80, 70);
-                        GUInew.gc.setFill(Color.YELLOW);
-                        GUInew.gc.fillOval(405, 72, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + geelcount, 445, 95);
-                        GUInew.gc.strokeRect(400, 375, 80, 70);
-                        GUInew.gc.fillText("Restbak", 410, 415);
-                        Thread.sleep(20);
+                        GUInew.gc.strokeLine(220, 225, 425, 145);
+                        GUInew.gc.strokeLine(220, 275, 425, 195);
+                        drawMachine();
                     }
+                    for (double i = 0; i < 80; i = i + 1.5) {
+                        r = r + 4;
+                        GUInew.gc.clearRect(0, 0, 5000, 5000);
+                        GUInew.gc.setFill(Color.RED);
+                        GUInew.gc.fillOval(r + 4, 232 - i, 36, 36);
+                        GUInew.gc.strokeLine(220, 225, 425, 145);
+                        GUInew.gc.strokeLine(220, 275, 425, 195);
+                        drawMachine();
+                    }
+                    roodcount++;
+                    r = 200;
                 }
                 if (inputLine.equals("geel")) {
-                    geelcount++;
-                    for (int i = 0; i < 500; i = i + 4) {
+                    for (int i = 0; i < 200; i = i + 4) {
                         GUInew.gc.clearRect(0, 0, 5000, 5000);
                         GUInew.gc.setFill(Color.YELLOW);
                         GUInew.gc.fillOval(i, 232, 36, 36);
-                        GUInew.gc.strokeRect(20, 200, 200, 100);
-                        GUInew.gc.strokeLine(220,225, 400, 65);
-                        GUInew.gc.strokeLine(220,275, 400, 115);
-                        //GUInew.gc.strokeRect(220, 225, 200, 50);
-                        GUInew.gc.strokeRect(450, 215, 80, 70);
-                        GUInew.gc.setFill(Color.GREEN);
-                        GUInew.gc.fillOval(455, 232, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + groencount, 495, 255);
-                        GUInew.gc.strokeRect(425, 135, 80, 70);
-                        GUInew.gc.setFill(Color.RED);
-                        GUInew.gc.fillOval(430, 152, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + roodcount, 470, 175);
-                        GUInew.gc.strokeRect(425, 295, 80, 70);
-                        GUInew.gc.setFill(Color.BLUE);
-                        GUInew.gc.fillOval(430, 312, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + blauwcount, 470, 335);
-                        GUInew.gc.strokeRect(400, 55, 80, 70);
-                        GUInew.gc.setFill(Color.YELLOW);
-                        GUInew.gc.fillOval(405, 72, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + geelcount, 445, 95);
-                        GUInew.gc.strokeRect(400, 375, 80, 70);
-                        GUInew.gc.fillText("Restbak", 410, 415);
-                        Thread.sleep(20);
+                        GUInew.gc.strokeLine(220, 225, 400, 70);
+                        GUInew.gc.strokeLine(220, 275, 400, 120);
+                        drawMachine();
                     }
+                    for (double i = 0; i < 160; i = i + 3.3) {
+                        r = r + 4;
+                        GUInew.gc.clearRect(0, 0, 5000, 5000);
+                        GUInew.gc.setFill(Color.YELLOW);
+                        GUInew.gc.fillOval(r + 4, 232 - i, 36, 36);
+                        GUInew.gc.strokeLine(220, 225, 400, 70);
+                        GUInew.gc.strokeLine(220, 275, 400, 120);
+                        drawMachine();
+                    }
+                    geelcount++;
+                    r = 200;
                 }
                 if (inputLine.equals("blauw")) {
-                    blauwcount++;
-                    for (int i = 0; i < 500; i = i + 4) {
+                    for (int i = 0; i < 200; i = i + 4) {
                         GUInew.gc.clearRect(0, 0, 5000, 5000);
                         GUInew.gc.setFill(Color.BLUE);
                         GUInew.gc.fillOval(i, 232, 36, 36);
-                        GUInew.gc.strokeRect(20, 200, 200, 100);
-                        GUInew.gc.strokeLine(220,225, 425, 305);
-                        GUInew.gc.strokeLine(220,275, 425, 355);
-                        //GUInew.gc.strokeRect(220, 225, 200, 50);
-                        GUInew.gc.strokeRect(450, 215, 80, 70);
-                        GUInew.gc.setFill(Color.GREEN);
-                        GUInew.gc.fillOval(455, 232, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + groencount, 495, 255);
-                        GUInew.gc.strokeRect(425, 135, 80, 70);
-                        GUInew.gc.setFill(Color.RED);
-                        GUInew.gc.fillOval(430, 152, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + roodcount, 470, 175);
-                        GUInew.gc.strokeRect(425, 295, 80, 70);
-                        GUInew.gc.setFill(Color.BLUE);
-                        GUInew.gc.fillOval(430, 312, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + blauwcount, 470, 335);
-                        GUInew.gc.strokeRect(400, 55, 80, 70);
-                        GUInew.gc.setFill(Color.YELLOW);
-                        GUInew.gc.fillOval(405, 72, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + geelcount, 445, 95);
-                        GUInew.gc.strokeRect(400, 375, 80, 70);
-                        GUInew.gc.fillText("Restbak", 410, 415);
-                        Thread.sleep(20);
+                        GUInew.gc.strokeLine(220, 225, 425, 305);
+                        GUInew.gc.strokeLine(220, 275, 425, 355);
+                        drawMachine();
                     }
+                    for (double i = 0; i < 80; i = i + 1.5) {
+                        r = r + 4;
+                        GUInew.gc.clearRect(0, 0, 5000, 5000);
+                        GUInew.gc.setFill(Color.BLUE);
+                        GUInew.gc.fillOval(r + 4, 232 + i, 36, 36);
+                        GUInew.gc.strokeLine(220, 225, 425, 305);
+                        GUInew.gc.strokeLine(220, 275, 425, 355);
+                        drawMachine();
+                    }
+                    r = 200;
+                    blauwcount++;
                 }
                 if (inputLine.equals("groen")) {
-                    groencount++;
                     for (int i = 0; i < 455; i = i + 4) {
                         GUInew.gc.clearRect(0, 0, 5000, 5000);
                         GUInew.gc.setFill(Color.GREEN);
                         GUInew.gc.fillOval(i, 232, 36, 36);
-                        GUInew.gc.strokeRect(20, 200, 200, 100);
-                        GUInew.gc.strokeLine(220,225, 450, 225);
-                        GUInew.gc.strokeLine(220,275, 450, 275);
-                        //GUInew.gc.strokeRect(220, 225, 200, 50);
-                        GUInew.gc.strokeRect(450, 215, 80, 70);
-                        GUInew.gc.setFill(Color.GREEN);
-                        GUInew.gc.fillOval(455, 232, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + groencount, 495, 255);
-                        GUInew.gc.strokeRect(425, 135, 80, 70);
-                        GUInew.gc.setFill(Color.RED);
-                        GUInew.gc.fillOval(430, 152, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + roodcount, 470, 175);
-                        GUInew.gc.strokeRect(425, 295, 80, 70);
-                        GUInew.gc.setFill(Color.BLUE);
-                        GUInew.gc.fillOval(430, 312, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + blauwcount, 470, 335);
-                        GUInew.gc.strokeRect(400, 55, 80, 70);
-                        GUInew.gc.setFill(Color.YELLOW);
-                        GUInew.gc.fillOval(405, 72, 36, 36);
-                        GUInew.gc.setFill(Color.BLACK);
-                        GUInew.gc.fillText("X " + geelcount, 445, 95);
-                        GUInew.gc.strokeRect(400, 375, 80, 70);
-                        GUInew.gc.fillText("Restbak", 410, 415);
-                        Thread.sleep(20);
+                        GUInew.gc.strokeLine(220, 225, 450, 225);
+                        GUInew.gc.strokeLine(220, 275, 450, 275);
+                        drawMachine();
                     }
+                    groencount++;
                 }
-            } catch (IOException | InterruptedException e) {
+            } catch (IOException e) {
             }
         }
     }
--- Communicator2.java
+++ Communicator2.java
@@ -9,12 +9,16 @@ import gnu.io.SerialPortEvent;
 import gnu.io.SerialPortEventListener;
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.paint.Color;
 
 public class Communicator2 implements SerialPortEventListener {
+
     SerialPort serialPort;
 
     //The port we're going to use.
-    private String PORT_NAME[] = { "COM5" };
+    private String PORT_NAME[] = {"COM6"};
 
     //A BufferedReader which will be fed by a InputStreamReader converting the bytes into characters
     BufferedReader input;
@@ -27,6 +31,12 @@ public class Communicator2 implements SerialPortEventListener {
 
     //Default bits per second for COM port.
     private int DATA_RATE = 9600;
+    
+    private void drawSetup() {
+        GUInew.gc.strokeLine(220, 225, 450, 225);
+        GUInew.gc.strokeLine(220, 275, 450, 275);
+        Communicator1.drawMachine();
+    }
 
     void initialize() {
         CommPortIdentifier portId = null;
@@ -75,49 +85,127 @@ public class Communicator2 implements SerialPortEventListener {
             try {
                 String inputLine = input.readLine();
                 Logboek.addRule(System.currentTimeMillis(), "(Arduino2): " + inputLine);
+                if (inputLine.equals("Dropping yellow ball")) {
+                    Communicator1.geelcount = Communicator1.geelcount - 1;
+                    for (int i = 0; i < 392; i = i + 4) {
+                        GUInew.gc.clearRect(0, 0, 5000, 5000);
+                        GUInew.gc.setFill(Color.YELLOW);
+                        if (i <= 292) {
+                            GUInew.gc.fillOval(480 + i, 72 + i / 2.5, 36, 36);
+                        } else if (i > 292) {
+                            GUInew.gc.fillOval(480 + i, 188.8, 36, 36);
+                        }
+                        drawSetup();
+                    }
+                    Communicator1.bakgeelcount = Communicator1.bakgeelcount + 1;
+                    GUInew.gc.clearRect(0, 0, 5000, 5000);
+                    drawSetup();
+                }
+                if (inputLine.equals("Dropping red ball")) {
+                    Communicator1.roodcount = Communicator1.roodcount - 1;
+                    for (int i = 0; i < 392; i = i + 4) {
+                        GUInew.gc.clearRect(0, 0, 5000, 5000);
+                        GUInew.gc.setFill(Color.RED);
+                        if (i <= 292) {
+                            GUInew.gc.fillOval(505 + i, 152 + i / 7.5, 36, 36);
+                        } else if (i > 292) {
+                            GUInew.gc.fillOval(505 + i, 190.93, 36, 36);
+                        }
+                        drawSetup();
+                    }
+                    Communicator1.bakroodcount = Communicator1.bakroodcount + 1;
+                    GUInew.gc.clearRect(0, 0, 5000, 5000);
+                    drawSetup();
+                }
+                if (inputLine.equals("Dropping green ball")) {
+                    Communicator1.groencount = Communicator1.groencount - 1;
+                    for (int i = 0; i < 352; i = i + 4) {
+                        GUInew.gc.clearRect(0, 0, 5000, 5000);
+                        GUInew.gc.setFill(Color.GREEN);
+                        if (i <= 268) {
+                            GUInew.gc.fillOval(530 + i, 232 - i / 6.5, 36, 36);
+                        } else if (i > 268) {
+                            GUInew.gc.fillOval(530 + i, 190.77, 36, 36);
+                        }
+                        drawSetup();
+                    }
+                    Communicator1.bakgroencount = Communicator1.bakgroencount + 1;
+                    GUInew.gc.clearRect(0, 0, 5000, 5000);
+                    drawSetup();
+                }
+                if (inputLine.equals("Dropping blue ball")) {
+                    Communicator1.blauwcount = Communicator1.blauwcount - 1;
+                    for (int i = 0; i < 392; i = i + 4) {
+                        GUInew.gc.clearRect(0, 0, 5000, 5000);
+                        GUInew.gc.setFill(Color.BLUE);
+                        if (i <= 292) {
+                            GUInew.gc.fillOval(505 + i, 295 - i / 2.8, 36, 36);
+                        } else if (i > 292) {
+                            GUInew.gc.fillOval(505 + i, 190.71, 36, 36);
+                        }
+                        drawSetup();
+                    }
+                    Communicator1.bakblauwcount = Communicator1.bakblauwcount + 1;
+                    GUInew.gc.clearRect(0, 0, 5000, 5000);
+                    drawSetup();
+                }
+                if (inputLine.equals("Pakje klaar!")) {
+                    Communicator1.pakjes = Communicator1.pakjes + 1;
+                    Communicator1.bakgroencount = 0;
+                    Communicator1.bakgeelcount = 0;
+                    Communicator1.bakblauwcount = 0;
+                    Communicator1.bakroodcount = 0;
+                    GUInew.gc.clearRect(0, 0, 5000, 5000);
+                    drawSetup();
+                }
+                if (inputLine.equals("Bestelling is klaar!")) {
+                    Communicator1.pakjes = 0;
+                    GUInew.gc.clearRect(0, 0, 5000, 5000);
+                    drawSetup();
+                }
+                
             } catch (IOException e) {
             }
         }
     }
-    
-    void YellowBalls(int i){
+
+    void YellowBalls(int t) {
         try {
-            output.write(i);
-        } catch (IOException e) {
-            Logboek.addRule(System.currentTimeMillis(), "ERROR: could not send input to Arduino2!");
-        }
-    }
-    
-    void RedBalls(int i){
-        try {
-            output.write(i);
-        } catch (IOException e) {
-            Logboek.addRule(System.currentTimeMillis(), "ERROR: could not send input to Arduino2!");
-        }
-    }
-    
-    void GreenBalls(int i){
-        try {
-            output.write(i);
-        } catch (IOException e) {
-            Logboek.addRule(System.currentTimeMillis(), "ERROR: could not send input to Arduino2!");
-        }
-    }
-    
-    void BlueBalls(int i){
-        try {
-            output.write(i);
-        } catch (IOException e) {
-            Logboek.addRule(System.currentTimeMillis(), "ERROR: could not send input to Arduino2!");
-        }
-    }
-    
-    void QuantityPackage(int i){
-        try {
-            output.write(i);
+            output.write(t);
         } catch (IOException e) {
             Logboek.addRule(System.currentTimeMillis(), "ERROR: could not send input to Arduino2!");
         }
     }
 
+    void RedBalls(int t) {
+        try {
+            output.write(t);
+        } catch (IOException e) {
+            Logboek.addRule(System.currentTimeMillis(), "ERROR: could not send input to Arduino2!");
+        }
+    }
+
+    void GreenBalls(int t) {
+        try {
+            output.write(t);
+        } catch (IOException e) {
+            Logboek.addRule(System.currentTimeMillis(), "ERROR: could not send input to Arduino2!");
+        }
+    }
+
+    void BlueBalls(int t) {
+        try {
+            output.write(t);
+        } catch (IOException e) {
+            Logboek.addRule(System.currentTimeMillis(), "ERROR: could not send input to Arduino2!");
+        }
+    }
+
+    void QuantityPackage(int i) {
+        try {
+            output.write(i);
+        } catch (IOException e) {
+            Logboek.addRule(System.currentTimeMillis(), "ERROR: could not send input to Arduino2!");
+        }
+    }
 }
